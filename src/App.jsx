3071f034accd55e804c84abc05bb510528ef2fc5
ROHIT SADHU <rohitsadhupersonal@gmail.com>
2024-03-28 12:24:09 +0530

    added browser only support

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,27 @@
 
 import React, { Suspense } from "react";
 import Loading from "./components/Loading";
+import { isIE, isMobile, MobileView, BrowserView } from 'react-device-detect';
 
 const Diagram = React.lazy(() => import("./Diagram"));
 
 function App() {
+  if (isIE) {
+    return <div> Internet Explorer(IE) is not supported. Download Chrome/Opera/Firefox </div>
+  }
+  if (isMobile) {
+    return (
+      <MobileView>
+        <div > This content is available only on large screens</div>
+      </MobileView>
+    )
+  }
   return (
+    <BrowserView>
     <Suspense fallback={<Loading />}>
       <Diagram />
     </Suspense>
+    </BrowserView>
   );
 }
 
